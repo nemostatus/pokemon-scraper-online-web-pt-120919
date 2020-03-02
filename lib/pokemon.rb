9482d5a60f5d99a3ba48ff20555deba33066350c
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-03-02 17:16:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,9 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db 
-  def initialize(keywords)
- 
+  def initialize(id=nil,name,type,db)
+ @id = id 
+ @name =name 
+ @type = type 
+ @db = db
 end
 end
